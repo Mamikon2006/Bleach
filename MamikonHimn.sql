4147d7d9efd730527507f3ea3966ebb7cf65aea0
Mamikon2006 <mamikon.k1.y@tumo.org>
2024-06-05 17:45:36 +0400

    Add files via upload

---
+++ MamikonHimn.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost:8889
+-- Время создания: Июн 05 2024 г., 08:02
+-- Версия сервера: 5.7.39
+-- Версия PHP: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `MamikonHimn`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `info` varchar(255) NOT NULL,
+  `price` text NOT NULL,
+  `photo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `info`, `price`, `photo`) VALUES
+(1, 'Product 1', 'Info about Product 1', '19.99', 'photo1.jpg'),
+(2, 'Product 2', 'Info about Product 2', '29.99', 'photo2.jpg'),
+(3, 'Product 3', 'Info about Product 3', '39.99', 'photo3.jpg'),
+(4, 'Product 4', 'Info about Product 4', '49.99', 'photo4.jpg'),
+(5, 'Product 5', 'Info about Product 5', '59.99', 'photo5.jpg');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
